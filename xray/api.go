16dbfb601b2ba7ca4ea65317c63fad618e9d44c8
Dgduducuccuccici <125131881+Dgduducuccuccici@users.noreply.github.com>
2026-06-07 23:55:08 +0630

    api.go

--- xray/api.go
+++ xray/api.go
@@ -7,8 +7,8 @@ import (
 	"regexp"
 	"time"
 
-	"github.com/nyeinkokoaung404/x-ui/logger"
-	"github.com/nyeinkokoaung404/x-ui/util/common"
+	"github.com/Dgduducuccuccici/x-ui/logger"
+	"github.com/Dgduducuccuccici/x-ui/util/common"
 
 	"github.com/xtls/xray-core/app/proxyman/command"
 	statsService "github.com/xtls/xray-core/app/stats/command"

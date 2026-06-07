7c3e2174cb070874759214c33e30e1de2a239c7e
Dgduducuccuccici <125131881+Dgduducuccuccici@users.noreply.github.com>
2026-06-08 00:10:02 +0630

    tgbot.go

--- web/service/tgbot.go
+++ web/service/tgbot.go
@@ -9,13 +9,13 @@ import (
 	"strings"
 	"time"
 
-	"github.com/nyeinkokoaung404/x-ui/config"
-	"github.com/nyeinkokoaung404/x-ui/database"
-	"github.com/nyeinkokoaung404/x-ui/database/model"
-	"github.com/nyeinkokoaung404/x-ui/logger"
-	"github.com/nyeinkokoaung404/x-ui/util/common"
-	"github.com/nyeinkokoaung404/x-ui/web/locale"
-	"github.com/nyeinkokoaung404/x-ui/xray"
+	"github.com/Dgduducuccuccici/x-ui/config"
+	"github.com/Dgduducuccuccici/x-ui/database"
+	"github.com/Dgduducuccuccici/x-ui/database/model"
+	"github.com/Dgduducuccuccici/x-ui/logger"
+	"github.com/Dgduducuccuccici/x-ui/util/common"
+	"github.com/Dgduducuccuccici/x-ui/web/locale"
+	"github.com/Dgduducuccuccici/x-ui/xray"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
